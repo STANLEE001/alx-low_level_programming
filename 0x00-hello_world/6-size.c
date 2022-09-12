f510ad633d52fdac32377220f9c0c6f033d02615
ogbonda stanley <ogbondastanley970@gmail.com>
2022-09-12 10:36:36 -0700

    assembler

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        char c;
+        int i;
+        long li;
+        long long lli;
+        float f;
+
+        print("size of a char: %ld byte(s)\n", sizeof(c));
+        print("size of an int: %ld byte(s)\n", sizeof(c));
